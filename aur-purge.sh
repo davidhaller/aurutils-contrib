d445c491cfa3c6ee265be30a944a3642d5aa4ee1
David Haller <haller_david@icloud.com>
2025-12-29 17:44:20 +0100

    Ignore rm error when trying to remove non-existent package

--- aur-purge.sh
+++ aur-purge.sh
@@ -4,7 +4,7 @@ aur_repo=$(aur repo --path)
 
 cd $(dirname $aur_repo)
 for pkg in $@; do
-    rm $(aur repo -F filename -s $pkg)
+    rm -f $(aur repo -F filename -s $pkg)
 done
 
 repo-remove $aur_repo $*
